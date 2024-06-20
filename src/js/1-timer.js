b61b4258e70cd5385024e6a5d39e062b000f50ee
victor <157064298+sundnepr@users.noreply.github.com>
2024-06-20 14:40:38 +0300

    update

---
+++ src/js/1-timer.js
@@ -0,0 +1,14 @@
+import flatpickr from "flatpickr";
+import iziToast from "izitoast";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(selector, options)
